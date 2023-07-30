d56734ca6eafe6b5ddd8e6b5039a85978a8806ea
Abolfazl <abolfazlheidari4036@gmail.com>
2023-07-30 18:32:27 +0330

    added new java scripts

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import ToodoCaard from "./components/ToodoCaard";
+
+function App() {
+  return (
+    <div>
+      <h1>My todos</h1>
+      <ToodoCaard title="sleeping"   />
+      <ToodoCaard title="coding" />
+      <ToodoCaard title="lunch" />
+      <ToodoCaard title="watching movie" />
+      <ToodoCaard title="disport" />
+      
+      
+   
+      
+    </div>
+  );
+}
+
+export default App;
